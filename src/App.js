77ee19ab684fde857acef6a80406533f3f341fad
Chaipitch Wongwangpaisarn <chaipitch6@gmail.com>
2022-01-28 14:46:25 +0700

    first edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  state = {latitude: null, errorMessage:''}
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({latitude: position.coords.latitude})
+      },
+      error => {
+        this.setState({errorMessage: error.message})
+      }
+    );
+  }
+
+  render(){
+    if(this.state.errorMessage && !this.state.latitude){
+      return <div>{this.state.error}</div>
+    }
+
+    if(!this.state.errorMessage && this.state.latitude){
+      return <div>{this.state.latitude}</div>
+    }
+
+    else{
+      return <div>Loading...</div>
+    }
+  } 
+  
 }
+
+
+
+
 
 export default App;
---
+++ src/components/HemisphereDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HemisphereDisplay() {
+  return <div>
+      You are in Northern Hemisphere
+  </div>;
+}
+
+export default HemisphereDisplay;
